de01d9a022e06ede456ccbff5ffe11933ca8cbd0
Tom Rice <t.rice90@gmail.com>
2012-06-15 03:10:17 -0700

    Developing the ability to plot flagged data differently.
    So far, I am able to handle lightcurves with either normal or info-flagged data.

--- tr/plot3.py
+++ tr/plot3.py
@@ -67,12 +67,18 @@ def lc (table, sid, season=0):
         print "no data here"
         return
 
+    ### Initializing variables. Most of the following are done thrice:
+    # Once with no errors (normal),
+    # once with small errors (info),
+    # once with large errors (warning).
+
+    ## First: normal
+
     # Use band_cut to get relevant data chunks.
-    
-    j_table = band_cut(s_table, 'j')
-    h_table = band_cut(s_table, 'h')
-    k_table = band_cut(s_table, 'k')
-#    jmh_table = band_cut(s_table, 'jmh')
+
+    j_table = band_cut(s_table, 'j', max_flag=0)
+    h_table = band_cut(s_table, 'h', max_flag=0)
+    k_table = band_cut(s_table, 'k', max_flag=0)
     hmk_table = band_cut(s_table, 'hmk')    
     # a quick hack to make JHK color-color/mag plots make sense:
     # (since J-H data is only ever used when plotted against H-K, 
@@ -113,7 +119,56 @@ def lc (table, sid, season=0):
     kflag = k_table.KPPERRBITS
     jmhflag = jmh_table.JPPERRBITS + jmh_table.HPPERRBITS
     hmkflag = hmk_table.HPPERRBITS + hmk_table.KPPERRBITS
+
+    ## Second: info
+
+    # Use band_cut to get relevant data chunks.
+
+    j_table_info = band_cut(s_table, 'j', min_flag=1, max_flag=256)
+    h_table_info = band_cut(s_table, 'h', min_flag=1, max_flag=256)
+    k_table_info = band_cut(s_table, 'k', min_flag=1, max_flag=256)
+    hmk_table_info = band_cut(s_table, 'hmk')    
+    # a quick hack to make JHK color-color/mag plots make sense:
+    # (since J-H data is only ever used when plotted against H-K, 
+    # and we need a second K column to use against H-K)
+    jmh_table_info = band_cut(hmk_table, 'jmh')
+    k_table_info2 = band_cut(k_table_info, 'hmk', min_flag=1, max_flag=256)
+
+
+    # The Julian date for CE  2000 January  1 00:00:00.0 UT is
+    # JD 2451544.500000
+    # (i.e. MJD 51544.0)
+
+    # get a date (x-axis) for each plot
+    jdate_info = j_table_info.MEANMJDOBS - 51544
+    hdate_info = h_table_info.MEANMJDOBS - 51544
+    kdate_info = k_table_info.MEANMJDOBS - 51544
+    jmhdate_info = jmh_table_info.MEANMJDOBS - 51544
+    hmkdate_info = hmk_table_info.MEANMJDOBS - 51544
     
+    # get a magnitude (y-axis) for each plot
+    jcol_info = j_table_info.JAPERMAG3
+    hcol_info = h_table_info.HAPERMAG3
+    kcol_info = k_table_info.KAPERMAG3
+    jmh =  jmh_table_info.JMHPNT
+    hmk =  hmk_table.HMKPNT
+    kcol_info2 = k_table_info2.KAPERMAG3 # Needed for the K vs H-K plot
+
+    # get a magnitude error (y-error) for each plot
+    jerr_info = j_table_info.JAPERMAG3ERR
+    herr_info = h_table_info.HAPERMAG3ERR
+    kerr_info = k_table_info.KAPERMAG3ERR
+    jmherr_info=jmh_table_info.JMHPNTERR
+    hmkerr_info=hmk_table_info.HMKPNTERR
+
+    # get a quality flag for each plot
+    jflag = j_table_info.JPPERRBITS
+    hflag = h_table_info.HPPERRBITS
+    kflag = k_table_info.KPPERRBITS
+    jmhflag = jmh_table_info.JPPERRBITS + jmh_table_info.HPPERRBITS
+    hmkflag = hmk_table_info.HPPERRBITS + hmk_table_info.KPPERRBITS
+    
+
     
     ## Define the components and parameters of the figure:
     
@@ -132,16 +187,33 @@ def lc (table, sid, season=0):
     ax_jhk = fig.add_axes( (.65, bottom, .3, .375) )
     ax_khk = fig.add_axes( (.65, bottom+.475, .3, .375) )
 
+    ## Start plotting. 
+    
+    fmt_info = 'D'
+    fmt_warn = 'k.'
+
     # Plot J-band:
-    ax_j.errorbar( jdate, jcol, yerr=jerr, fmt='bo', ecolor='k')
+    if len(jdate) > 0:
+        ax_j.errorbar( jdate, jcol, yerr=jerr, fmt='bo', ecolor='k')
+    if len(jdate_info) > 0:
+        ax_j.errorbar( jdate_info, jcol_info, yerr=jerr_info, 
+                       fmt='b'+fmt_info, ms=4, ecolor='k')
     ax_j.invert_yaxis()
 
     # Plot H-band:
-    ax_h.errorbar( hdate, hcol, yerr=herr, fmt='go', ecolor='k' )
+    if len(hdate) > 0:
+        ax_h.errorbar( hdate, hcol, yerr=herr, fmt='go', ecolor='k' )
+    if len(hdate_info) > 0:
+        ax_h.errorbar( hdate_info, hcol_info, yerr=herr_info, 
+                       fmt='g'+fmt_info, ms=4, ecolor='k')
     ax_h.invert_yaxis()
 
     # Plot K-band:
-    ax_k.errorbar( kdate, kcol, yerr=kerr, fmt='ro', ecolor='k' )
+    if len(kdate) > 0:
+        ax_k.errorbar( kdate, kcol, yerr=kerr, fmt='ro', ecolor='k' )
+    if len(kdate_info) > 0:
+        ax_k.errorbar( kdate_info, kcol_info, yerr=kerr_info, 
+                       fmt='r'+fmt_info, ms=4, ecolor='k')
     ax_k.invert_yaxis()
 
     # Plot J-H vs H-K
@@ -150,8 +222,11 @@ def lc (table, sid, season=0):
 
     # Plot K vs H-K
     # Note: not sure if this is going to break or not.
-    plot_trajectory_core( ax_khk, hmk, kcol2, hmkdate , ms=False, ctts=False) # gonna update this so that it properly uses K-band main sequence line?
-    ax_khk.invert_yaxis()
+    try:
+        plot_trajectory_core( ax_khk, hmk, kcol2, hmkdate , ms=False, ctts=False) # gonna update this so that it properly uses K-band main sequence line?
+    except:
+        pass
+        ax_khk.invert_yaxis()
 
     # Hide the bad labels...
     plt.setp(ax_j.get_xticklabels(), visible=False)
