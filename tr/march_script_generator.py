d9839f7fa0b65f1daab99a20f7f4adf5154cef24
Tom Rice <t.rice90@gmail.com>
2013-03-04 21:07:20 -0800

    Did a crazy amount of redesigning. Current version makes non-phased LCs.

--- tr/march_script_generator.py
+++ tr/march_script_generator.py
@@ -25,10 +25,11 @@ import atpy
 import plot3, spread3
 
 from cygob7_jjh_script_helper import *
+from super import mkdir_p
 
 # where the data is stored
 path = '/home/tom/Dropbox/Cyg_OB7/paper2/'
-path2 ='/home/tom/Dropbox/Cyg_OB7/paper2/book/March2013/' 
+lc_path ='/home/tom/Dropbox/Cyg_OB7/paper2/book/March2013/' 
 path3 ='/home/tom/reu/DATA/Merged_Catalogs/September_2011/'
 
 # The data file that contains data for all of the wise disks, aspin sources,
@@ -73,7 +74,7 @@ ras = aspin_sources > 1
 rwt = wise_trans > 1
 rwe = wise_extras > 1
 
-def do_it_all_cygob7():
+def do_it_all_cygob7(lc=True):
     """
     Creates Lightcurve plots for WISE, Aspin, and RWA sources.
 
@@ -81,67 +82,129 @@ def do_it_all_cygob7():
     
     """
 
-    # wise disks
-    for s, n in zip(wise_disks[rwd], wise_disks_names[rwd]):
-        fig = plot3.jjh(watso, s, name=str(n), color_slope=True, 
-                        date_offset=54579,
-                        outfile=path2+'Wise/'+str(n), png_too=True)
+    # Preliminary stuff
+    ## First, make directories for everything (or try, at least).
 
-        if fig == None:
-            print "dude %s failed to plot right" % str(s)
+    # Structure of directories:
+    #  lc_path/lc
+    #  lc_path/phase
+    #  lc_path/tables
+    # within each of those (now it's a *),
+    #  lc_path/*/(s1 | s2 | s3 | s123)
+    # Within lc/s*, we put the files directly in.
+    # Within phase/s*, we do
+    #  lc_path/phase/s*/(h_fx2 | h_lsp | j_fx2 | j_lsp | k_fx2 | k_lsp | lsp_power)
+    # Within tables/s*, we put the files directly in.
 
-    # wise special case
-    for s, n in zip(wise_v, wise_v_names):
-        fig = plot3.jjh(data, s, name=str(n), color_slope=True, 
-                        date_offset=54579,
-                        outfile=path2+'Wise/'+str(n), png_too=True)
+    # mkdir_p(lc_path+"lc/")
+    # mkdir_p(lc_path+"phase/")
+    # mkdir_p(lc_path+"tables/")
 
+    ss = ['s1', 's2', 's3', 's123']
+    # types = ['h_fx2', 'h_lsp', 'j_fx2', 'j_lsp', 'k_fx2', 'k_lsp', 'lsp_power']
+    groups = ['Wise', 'Aspin', 'transition', 'Wise_extras', 'RWA_sources']
 
-    # aspins
-    for s, n in zip(aspin_sources[ras], aspin_sources_names[ras]):
-        fig = plot3.jjh(watso, s, name=str(n), color_slope=True, 
-                        date_offset=54579,
-                        outfile=path2+'Aspin/'+str(n), png_too=True)
+    # for s in ss:
+    #     mkdir_p(lc_path+"lc/"+s)
+    #     mkdir_p(lc_path+"tables/"+s)
+    #     for t in types:
+    #         mkdir_p("%sphase/%s/%s"%(lc_path,s,t))
 
-        if fig == None:
-            print "dude %s failed to plot right" % str(s)
-
-
-    # wise trans
-    for s, n in zip(wise_trans[rwt], wise_trans_names[rwt]):
-        fig = plot3.jjh(watso, s, name=str(n), color_slope=True, 
-                        date_offset=54579,
-                        outfile=path2+'transition/'+str(n), png_too=True)
-
-        if fig == None:
-            print "dude %s failed to plot right" % str(s)
-
-    # wise extras
-    for s, n in zip(wise_extras[rwe], wise_extras_names[rwe]):
-        fig = plot3.jjh(data, s, name=str(n), color_slope=True, 
-                        date_offset=54579,
-                        outfile=path2+'Wise_extras/'+str(n), png_too=True)
-
-        if fig == None:
-            fig2 = plot3.jjh(welo, s, name=str(n), color_slope=True, 
-                             date_offset=54579,
-                             outfile=path2+'Wise_extras/'+str(n), png_too=True)
-            if fig2 == None:
-                print "dude %s failed to plot right" % str(s)
-
-    print "Now for RWA sources:"
-
-    for s, n in zip(rwa_sources, rwa_names):
-        fig = plot3.jjh(rwa_data, s, name=str(n), color_slope=True, 
-                        date_offset=54579,
-                        outfile=path2+'RWA_sources/'+str(n), png_too=True)
-
-        if fig == None:
-            print "dude %s failed to plot right" % str(s)
+    for g in groups:
+        for s in ss:
+            mkdir_p(lc_path+g+"/lc/"+s)
+            
+    # We should now be done making directories. Let's test this.
+    # tested!
 
 
-    print "did that."
+    # First: do all the straight lightcurves
+
+    if lc:
+        for season, s_name in zip([1,2,3,123], ss):
+
+            if season < 3: continue
+            print season, "is the current season"
+
+            # wise disks
+            for s, n in zip(wise_disks[rwd], wise_disks_names[rwd]):
+                fig = plot3.lc(watso, s, season=season, name=str(n), color_slope=True, 
+                               date_offset=54579, 
+                               custom_xlabel="Time (JD since 04/23/2008)",
+                               plot_warn=False,
+                               outfile=lc_path+'Wise/lc/'+s_name+'/'+str(n), 
+                               png_too=True)
+
+                if fig == None:
+                    print "dude %s failed to plot right" % str(s)
+
+
+            # wise special case
+            for s, n in zip(wise_v, wise_v_names):
+                fig = plot3.lc(data, s, season=season,
+                               name=str(n), color_slope=True, 
+                               date_offset=54579,
+                               outfile=lc_path+'Wise/lc/'+s_name+'/'+str(n),
+                               png_too=True)
+
+
+
+            # aspins
+            for s, n in zip(aspin_sources[ras], aspin_sources_names[ras]):
+                fig = plot3.lc(watso, s, season=season,
+                               name=str(n), color_slope=True, 
+                               date_offset=54579,
+                               outfile=lc_path+'Aspin/lc/'+s_name+'/'+str(n), 
+                               png_too=True)
+
+                if fig == None:
+                    print "dude %s failed to plot right" % str(s)
+
+
+            # wise trans
+            for s, n in zip(wise_trans[rwt], wise_trans_names[rwt]):
+                fig = plot3.lc(watso, s, season=season, 
+                               name=str(n), color_slope=True, 
+                               date_offset=54579,
+                               outfile=lc_path+'transition/lc/'+s_name+'/'+str(n), 
+                               png_too=True)
+
+                if fig == None:
+                    print "dude %s failed to plot right" % str(s)
+
+            # wise extras
+            for s, n in zip(wise_extras[rwe], wise_extras_names[rwe]):
+                fig = plot3.lc(data, s, season=season,
+                               name=str(n), color_slope=True, 
+                               date_offset=54579,
+                               outfile=lc_path+'Wise_extras/lc/'+s_name+'/'+str(n),
+                               png_too=True)
+
+                if fig == None:
+                    fig2 = plot3.lc(welo, s, season=season,
+                                    name=str(n), color_slope=True, 
+                                    date_offset=54579,
+                                    outfile=lc_path+'Wise_extras/lc/'+s_name+'/'+str(n),
+                                    png_too=True)
+                    if fig2 == None:
+                        print "dude %s failed to plot right" % str(s)
+
+            print "Now for RWA sources:"
+
+            for s, n in zip(rwa_sources, rwa_names):
+                fig = plot3.lc(rwa_data, s, season=season,
+                               name=str(n), color_slope=True, 
+                               date_offset=54579,
+                               outfile=lc_path+'RWA_sources/lc/'+s_name+'/'+str(n), 
+                               png_too=True)
+
+                if fig == None:
+                    print "dude %s failed to plot right" % str(s)
+
+
+        print "did that."
 
     
+
 
 
