075551c422387d0822de80dfa8eb364fdd0e8998
Tom Rice <t.rice90@gmail.com>
2013-11-21 14:57:39 -0500

    Added a test for ONCvar 8 (has flagged data) and named the rest ONCvar 1.

--- tr/test_plot_refactor.py
+++ tr/test_plot_refactor.py
@@ -9,30 +9,37 @@ import plot3
 
 def test_1():
     plot3.lc(variables_photometry, ukvar_spread.SOURCEID[0], 
-             name='lc: default settings')
+             name='lc: default settings, ONCvar 1')
 
     pass # will be "new plot"
 
 def test_2():
     plot3.graded_lc(variables_photometry, ukvar_spread.SOURCEID[0], 
-                    name='graded_lc: default settings')
+                    name='graded_lc: default settings, ONCvar 1')
 
     pass # will be "new plot"
 
 def test_3():
     plot3.lc(variables_photometry, ukvar_spread.SOURCEID[0], color_slope=True,
-             name='lc: color_slope=True')
+             name='lc: color_slope=True, ONCvar 1')
 
     pass # will be "new plot"    
 
 def test_4():
     plot3.graded_lc(variables_photometry, ukvar_spread.SOURCEID[0], 
                     color_slope=True,
-                    name='graded_lc: color_slope=True')
+                    name='graded_lc: color_slope=True, ONCvar 1')
 
     pass # will be "new plot"
 
-tests = [test_1, test_2, test_3, test_4]
+def test_5():
+    plot3.graded_lc(variables_photometry, ukvar_spread.SOURCEID[7], 
+                    color_slope=True,
+                    name='graded_lc: color_slope=True, ONCvar 8')
+    
+    pass # will be "new plot"
+
+tests = [test_1, test_2, test_3, test_4, test_5]
 
 for t in tests:
     t()
