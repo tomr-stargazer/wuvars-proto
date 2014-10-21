a98be765955f348a2faf780f90057b4698e2e7f5
Tom Rice <t.rice90@gmail.com>
2014-10-20 21:30:58 -0400

    Added a kwarg to plot_trajectory_core that gives me more flexibility.

--- tr/plot2.py
+++ tr/plot2.py
@@ -100,7 +100,8 @@ def plot_trajectory_vanilla (ax, a_k=1, ctts=True):
 # Change the colorbar call to the figure method (figure it out) [ ]
 def plot_trajectory_core (ax, hmk, jmh, c, cmap='jet', label="Time",
                           fmt='k.', ms_hmk=ms_hmk, ms_jmh=ms_jmh, a_k=1,
-                          ms=True, ctts=True, colorbar=True, s=10, **kwargs):
+                          ms=True, ctts=True, colorbar=True, s=10, edgecolors='none',
+                          **kwargs):
     """ Plots the trajectory of some star in color-color space.
     
     Inputs:
@@ -144,7 +145,7 @@ def plot_trajectory_core (ax, hmk, jmh, c, cmap='jet', label="Time",
 
     # Then, plot the actual data we were given
     sc = ax.scatter(hmk, jmh, c=c, marker='o', s=s, 
-                    cmap=cmap, edgecolors='none', **kwargs)
+                    cmap=cmap, edgecolors=edgecolors, **kwargs)
     if colorbar:
         cbar = plt.gcf().colorbar(sc, ax=ax) # This should really be changed to the method
         cbar.set_label(label)
